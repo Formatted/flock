cd5a7277bf89474f637921b888a3c38ce6ef2869
formatted <ronivegh@gmail.com>
2017-03-31 19:33:10 -0600

    we get the diff

--- src/speed.js
+++ src/speed.js
@@ -13,17 +13,17 @@ class Speed extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      timing: 3600000,
-      timing2: 60000,
+      timing: 1000 * 60 * 60,
+      timing2: 1000 * 60,
+      start: '',
+      stop: '',
+      diff: '',
     }
   }
   // componentDidMount () {
   // }
   _slowDown = () => {
     let foo = this.state.timing2
-    Alert.alert(
-      'boo ' + foo,
-    );
     this.setState({ timing2: foo + 1000 })
   };
 
@@ -32,17 +32,31 @@ class Speed extends React.Component {
     if(foo < 1001){
       foo += 1000
     }
-    Alert.alert(
-      'boo ' + foo,
-    );
     this.setState({ timing2: foo - 1000 })
   };
 
+  _start = () => {
+    let foo = new Date()
+    this.setState({ start: foo })
+  }
+
+  _stop = () => {
+    let foo = new Date()
+    let boo = this.state.start
+    this.setState({ stop: foo })
+    this.setState({ start: '' })
+    if(this.state.start !== '' && foo > boo){
+      this.setState({ diff:  foo - boo })
+    }
+  }
 
     render() {
 
       return (
         <View style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <Text>Speed1: {this.state.timing / 1000} in seconds</Text>
+          <Text>Speed2: {this.state.timing2 / 1000} in seconds</Text>
+          <Text>And here is your diff: {this.state.diff / 1000}</Text>
           <Button
             title="Slow down"
             onPress={this._slowDown}
@@ -50,6 +64,14 @@ class Speed extends React.Component {
           <Button
             title="Speed up"
             onPress={this._speedUp}
+          />
+          <Button
+            title="Start"
+            onPress={this._start}
+          />
+          <Button
+            title="Stop"
+            onPress={this._stop}
           />
           <View style={styles.container}>
             <Spinner timing={this.state.timing} />
